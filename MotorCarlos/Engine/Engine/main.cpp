fd2ec8a187ae1bafb532b59f53cdfd87a26fac54
Clantanab <73425709+Clantanab@users.noreply.github.com>
2022-05-17 13:14:08 +0200

    rrr

--- MotorCarlos/Engine/Engine/main.cpp
+++ MotorCarlos/Engine/Engine/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 
-#include <SFML\Window.hpp>
-#include <SFML\Graphics.hpp>
 #include <ciso646>
 #include <memory>
 #include <vector>
+#include <include\SDL.h>
+#include <GLFW\glfw3.h>
 
-using namespace sf;
+
 int main() 
 {
-	constexpr auto window_width = 800u;
-	constexpr auto window_height = 600u;
-	RenderWindow window(VideoMode(window_width, window_height), "Box2D Forces", Style::Titlebar | Style::Close, ContextSettings(32));
-	//window.setVerticalSyncEnabled(true);
-
+	int width = 80;
+	int height = 80;
+	SDL_Window* window = SDL_CreateWindow("Prueba", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 	
 	return 0;
 }
