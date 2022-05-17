d5864c4b4e69babed18f5f1f5c170e1435d34fb6
Clantanab <73425709+Clantanab@users.noreply.github.com>
2022-05-17 18:04:31 +0200

    ffff

--- MotorCarlos/Engine/Engine/main.cpp
+++ MotorCarlos/Engine/Engine/main.cpp
@@ -4,7 +4,7 @@
 #include <memory>
 #include <vector>
 #include <include\SDL.h>
-#include <GLFW\glfw3.h>
+//#include <GLFW\glfw3.h>
 
 
 int main() 
@@ -12,6 +12,10 @@ int main()
 	int width = 80;
 	int height = 80;
 	SDL_Window* window = SDL_CreateWindow("Prueba", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+
+
+
 	
 	return 0;
 }
