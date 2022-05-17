2971e25f6a325a129d113be41be2f245fb3541a0
Clantanab <73425709+Clantanab@users.noreply.github.com>
2022-05-17 20:01:55 +0200

    window

--- MotorCarlos/Engine/Engine/main.cpp
+++ MotorCarlos/Engine/Engine/main.cpp
@@ -3,17 +3,19 @@
 #include <ciso646>
 #include <memory>
 #include <vector>
-#include <include\SDL.h>
+
+
+
 #undef main
 //#include <GLFW\glfw3.h>
 
 
 int main() 
 {
-	int width = 800;
+	/*int width = 800;
 	int height = 800;
 	SDL_Window* window = SDL_CreateWindow("Prueba", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-
+	*/
 
 
 
---
+++ MotorCarlos/Engine/Engine/window.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#include <include\SDL.h>
+#include <ciso646>
+#include <iostream>
+
+namespace 
+{
+
+
+	class Window {
+
+
+	public:
+		size_t width;
+		size_t heigth;
+
+		SDL_Window* window;
+
+		SDL_GLContext context;
+
+
+		Window(const std::string& name, size_t  width, size_t height, bool allScreen = false);
+
+		unsigned Get_Width() const;
+		unsigned Get_Height() const;
+
+		void Clear() const;
+		void SwapBuffers() const;
+		void HandleEvents();
+
+		void End();
+
+
+	};
+}
