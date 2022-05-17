2a4c412e3e667f235a62f318b862066a9d46e033
Clantanab <73425709+Clantanab@users.noreply.github.com>
2022-05-17 11:58:49 +0200

    proegreso

--- MotorCarlos/Engine/Engine/main.cpp
+++ MotorCarlos/Engine/Engine/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
-#include <sfml-2.5.1\include\SFML\Window.hpp>
-#include <sfml-2.5.1\include\SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Graphics.hpp>
+#include <ciso646>
+#include <memory>
+#include <vector>
 
+using namespace sf;
 int main() 
 {
 
 	RenderWindow window(VideoMode(window_width, window_height), "Box2D Forces", Style::Titlebar | Style::Close, ContextSettings(32));
 
-
+	
 	return 0;
 }
