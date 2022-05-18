5b28323b18bc6cbd9059378b58f1357ddd81a055
Clantanab <73425709+Clantanab@users.noreply.github.com>
2022-05-18 10:13:44 +0200

    a

--- MotorCarlos/Engine/code/source/main.cpp
+++ MotorCarlos/Engine/code/source/main.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
+#include <window.h>
 
+using namespace engine;
+int main() 
+{
+
+
+
+	Window window = Window("prueba", 800, 800, false);
+
+	do {
+
+	} while (true);
+
+	return 0;
+}
--- MotorCarlos/Engine/code/source/window.cpp
+++ MotorCarlos/Engine/code/source/window.cpp
@@ -9,43 +9,64 @@ namespace engine {
 
 	Window::Window(const std::string& name, size_t  width, size_t height, bool allScreen = false)
 	{
-
-		context = nullptr;
-
+		
 		this->width = width;
 		this->heigth = height;
+		context = nullptr;
 
+
+		//Comprobamos el correcto init
 		if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		{
-			SDL_Log("Error SDL2.");
+			SDL_Log("SDL no se ha iniciado correctamente");
 		}
-		else
+		else //Si no hay error creamos una ventana
 		{
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 			window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 		}
 
 		if (not window)
 		{
-			SDL_Log("Error window.");
+			SDL_Log("Problema con la creacion de la ventana");
 		}
 		else
 		{
-
+			//Seteamos el contexto 
 			context = SDL_GL_CreateContext(window);
 
 			assert(context != nullptr);
 
 			if (context && glt::initialize_opengl_extensions())
 			{
-				if (allScreen)
-				{
-					SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
-				}
+				if (allScreen) SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP); //Maximizamos la vetana
 			}
 		}
+	}
+
+	unsigned Window::Get_Width() const 
+	{
+		return this->width;
+	}
+	unsigned Window::Get_Height() const
+	{
+		return this->heigth;
+	}
+
+	void Window::Clear()const
+	{
+		if(context)glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+	void Window::SwapBuffers() const 
+	{
+		if (context) SDL_GL_SwapWindow(this->window);
+	}
+
+	void Window::HandleEvents() 
+	{
+
 	}
 
 }
