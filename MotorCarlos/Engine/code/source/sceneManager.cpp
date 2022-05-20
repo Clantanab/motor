634775ae3b79c712d0e94004ccb9ac7f0bc87057
Clantanab <73425709+Clantanab@users.noreply.github.com>
2022-05-20 18:46:11 +0200

    final

--- MotorCarlos/Engine/code/source/sceneManager.cpp
+++ MotorCarlos/Engine/code/source/sceneManager.cpp
@@ -128,19 +128,31 @@ namespace engine
 			}
 		}
 	}
-
+	//Esta funcion solo sirve para la creacion de una escena demo default 
 	void SceneManager::CreateSceneDemo()
 	{
+		//creacion de ventana y escena
 		Window* window = new Window("prueba", 800, 800, false);
 		Scene* scene = new Scene("deMo", *window);
 
 		glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
 		std::string playerTag = "player";
+
+		//Creacion de las entidades de platyer, luz y camara
 		Entity* e = new Entity(scene, playerTag);
 		Entity* e1 = new Entity(scene);
 		Entity* e2 = new Entity(scene);
+
+
+		//Creacion de las entidades enemigo
 		std::string enemy1Tag = "enemy";
+		std::string enemy2Tag = "enemy2";
+		std::string enemy3Tag = "enemy3";
+		std::string enemy4Tag = "enemy4";
 		Entity* e3 = new Entity(scene, enemy1Tag);
+		Entity* e4 = new Entity(scene, enemy2Tag);
+		Entity* e5 = new Entity(scene, enemy3Tag);
+		Entity* e6 = new Entity(scene, enemy4Tag);
 
 
 
@@ -150,27 +162,57 @@ namespace engine
 
 		int i = 1;
 
-
+		//Creacion de las meses
 		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateMeshComponent(e, path);
 		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateMeshComponent(e3, path);
+		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateMeshComponent(e4, path);
+		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateMeshComponent(e5, path);
+		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateMeshComponent(e6, path);
+
+		//se añade el componente jugador a una entidad
 		SceneManager::Instance().GetScene("deMo")->updateSystem->AddPlayerComponent(e, 0.030f, *SceneManager::Instance().GetScene(0)->inputSystem);
-		e->GetTransform()->Translate(Vector3(-6, 0, -10));
+
+		//colocamos todas las entidaddes en sus posiciones iniciales
+		e->GetTransform()->Translate(Vector3(0, 0, -10));
 		e3->GetTransform()->Translate(Vector3(6, 0, -10));
 		e3->GetTransform()->SetScale(Vector3(2, 2, 2));
+		e4->GetTransform()->Translate(Vector3(0, 6, -10));
+		e4->GetTransform()->SetScale(Vector3(2, 2, 2));
+		e5->GetTransform()->Translate(Vector3(0, -6, -10));
+		e5->GetTransform()->SetScale(Vector3(2, 2, 2));
+		e6->GetTransform()->Translate(Vector3(-6, 0, -10));
+		e6->GetTransform()->SetScale(Vector3(2, 2, 2));
+
+		//añadimos los componentes enemigos 
 		SceneManager::Instance().GetScene("deMo")->updateSystem->AddEnemyComponent(e3, 0.01f, e->GetTransform());
+		SceneManager::Instance().GetScene("deMo")->updateSystem->AddEnemyComponent(e4, 0.01f, e->GetTransform());
+		SceneManager::Instance().GetScene("deMo")->updateSystem->AddEnemyComponent(e5, 0.01f, e->GetTransform());
+		SceneManager::Instance().GetScene("deMo")->updateSystem->AddEnemyComponent(e6, 0.01f, e->GetTransform());
+
+		//añadimos el componente camara y luz y las colocamos en la escena
 		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateCameraComponent(e1);
 		//SceneManager::Instance().GetScene(0)->updateSystem->AddPlayerComponent(&e1, 0.0050f, *SceneManager::Instance().GetScene(0)->inputSystem);
 		SceneManager::Instance().GetScene("deMo")->rendererSystem->CreateLightComponent(e2);
 		e2->GetTransform()->Translate(Vector3(10.f, 10.f, 10.f));
+
+		//marcamos la escena como una escena activa (inicia el kernel)
 		SceneManager::Instance().ActivateScene("deMo");
 	}
 
+	//Esta funcion sirve para resetear la escena demo inicial
 	void SceneManager::ResetSceneDemo()
 	{
 		std::string playerTag = "player";
 		std::string enemy1Tag = "enemy";
-		SceneManager::Instance().GetScene("deMo")->GetEntity(&playerTag)->GetTransform()->SetPosition(Vector3(-6, 0, -10));
+		std::string enemy2Tag = "enemy2";
+		std::string enemy3Tag = "enemy3";
+		std::string enemy4Tag = "enemy4";
+
+		SceneManager::Instance().GetScene("deMo")->GetEntity(&playerTag)->GetTransform()->SetPosition(Vector3(0, 0, -10));
 		SceneManager::Instance().GetScene("deMo")->GetEntity(&enemy1Tag)->GetTransform()->SetPosition(Vector3(6, 0, -10));
+		SceneManager::Instance().GetScene("deMo")->GetEntity(&enemy2Tag)->GetTransform()->SetPosition(Vector3(-6, 0, -10));
+		SceneManager::Instance().GetScene("deMo")->GetEntity(&enemy3Tag)->GetTransform()->SetPosition(Vector3(0, 6, -10));
+		SceneManager::Instance().GetScene("deMo")->GetEntity(&enemy4Tag)->GetTransform()->SetPosition(Vector3(0, -6, -10));
 
 	}
 }
